462419bc4227c10157ada1b3f7457a0d1d21e21c
juqkai <juqkai@gmail.com>
2021-11-23 13:17:49 +0800

    解决CORS问题

--- README.md
+++ README.md
@@ -28,11 +28,9 @@
 
 ### 上手指南
 
-1. 下载 [obsidian-proxy-github.zip](https://gitee.com/juqkai/obsidian-proxy-github/attach_files/878763/download/obsidian-proxy-github.zip)
+1. 下载 [obsidian-proxy-github.zip](https://gitee.com/juqkai/obsidian-proxy-github/releases)
 2. 解压 obsidian-proxy-github.zip
 3. 将解压的文件夹放入笔记目录下的插件目录内。如：XXX/.obsidian/plugins
-4. 如果是第一次使用这个插件，需要执行 【XXX/.obsidian/plugins/obsidian-proxy-github/ObsidianLink.bat】在桌面创建一个快捷方式。作用是取消安全限制，不然会因为跨域问题无法下载插件。
-5. 此后，如果需要安装插件时，需要运行桌面上新创建的 Obsidian 快捷方式
 
 
 ### 作者
--- main.js
+++ main.js
@@ -1,35 +1,76 @@
-const {Plugin} = require("obsidian");
+const { Plugin } = require("obsidian");
+
+
 let include = [
     {
         match: (url) => url.indexOf("/releases/download/") >= 0
-        ,to: (url) => url.replace("https://github.com/", "https://download.fastgit.org/")
+        , to: (url) => url.replace("https://github.com/", "https://download.fastgit.org/")
     }
     , {
         match: (url) => url.startsWith("https://raw.githubusercontent.com/") >= 0
-        ,to: (url) => url.replace("https://raw.githubusercontent.com/", "https://raw.fastgit.org/")
+        , to: (url) => url.replace("https://raw.githubusercontent.com/", "https://raw.fastgit.org/")
     }
     , {
         match: (url) => url.startsWith("https://github.com/") >= 0
-        ,to: (url) => url.replace("https://github.com/", "https://hub.fastgit.org/")
+        , to: (url) => url.replace("https://github.com/", "https://hub.fastgit.org/")
     }
 ]
-let ap;
 
-module.exports = class ProxyGithub extends Plugin {
-    onload(){
-        ap = window.ajaxPromise;
-        window.ajaxPromise = function(e){
-            for(var key in include){
-                let item = include[key]
-                if(item.match(e.url)){
-                    e.url = item.to(e.url)
-                    break
-                }
+// 匹配URL
+function matchUrl(e){
+    for (var key in include) {
+        let item = include[key]
+        if (item.match(e.url)) {
+            e.url = item.to(e.url)
+            if (!e.headers) {
+                e.headers = {}
             }
-            return ap(e)
+            e.headers["content-type"] = "application/x-www-form-urlencoded";
+            e.headers["Access-Control-Allow-Origin"] = "*"
+            return true;
         }
     }
-    onunload(){
+    return false;
+}
+
+// 代理访问
+function proxy(e){
+    return new Promise((function (t, n) {
+        e.success = t;
+        e.error = function (e, t) {
+            return n(t)
+        }
+        const https = require('https')
+        https.get(e.url, function (res) {
+            res.setEncoding('utf8');
+            let rawData = '';
+            res.on('data', (chunk) => { rawData += chunk; });
+            res.on('end', () => {
+                try {
+                    e.success(rawData)
+                } catch (e) {
+                    e.error(e)
+                }
+            });
+
+        }).on('error', function (e) {
+            e.error(e)
+        })
+    }))
+}
+
+let ap;
+module.exports = class ProxyGithub extends Plugin {
+    onload() {
+        ap = window.ajaxPromise;
+        window.ajaxPromise = function (e) {
+            if (!matchUrl(e)) {
+                return ap(e);
+            }
+            return proxy(e)
+        }
+    }
+    onunload() {
         window.ajaxPromise = ap;
     }
 }
